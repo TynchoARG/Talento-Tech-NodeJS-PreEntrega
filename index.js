3cd39639a2924df3522967d78ee40cd48192c83b
Tyncho <tyncho.zabala@gmail.com>
2026-09-22 13:00:32 -0300

    inicio del index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.clear() //Limnpia la terminal
+//console.clear() //Limnpia la terminal
 console.log("Inicio del CRUD...")
 
 //conexion con la API FakeStore
 
+async function getProducts() {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products", { method: "GET" });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Conexion exitosa")
+            return data
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
