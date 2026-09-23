4b95409fd5bf76230ecbf08007ef4f9e747606f9
Tyncho <tyncho.zabala@gmail.com>
2026-09-23 12:42:19 -0300

    todavia no funciona index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 console.log("Inicio del CRUD...")
 
 //conexion con la API FakeStore
-
+/*
 async function getProducts() {
     try {
         const response = await fetch("https://fakestoreapi.com/products", { method: "GET" });
@@ -14,4 +14,29 @@ async function getProducts() {
     } catch (error) {
         console.log(error)
     }
+}
+*/
+
+const args =  process.argv.slice(2);
+
+switch (args[0]) {
+    case "GET":
+        async function getProducts() {
+            try{
+                const response = await fetch("https://fakestoreapi.com/products", {method:"GET"});
+                if (response.ok){
+                    const data = await response.json();
+                    console.log("Conexion Exitosa");
+                    return data
+                }
+            } catch (error){
+                console.log(error)
+            }
+        }
+        break;
+    case "GET products/":
+        break;
+    default:
+        console.log("Coamndo incorrecto o incompleto");
+
 }
